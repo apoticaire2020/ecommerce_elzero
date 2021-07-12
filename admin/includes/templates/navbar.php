648a2040bc11165c50f9176a1b70ac3bcb5f4595
apoticaire2020 <andalossi2008@gmail.com>
2021-07-12 19:47:59 +0100

    bootstarp navbar

---
+++ admin/includes/templates/navbar.php
@@ -0,0 +1,35 @@
+<nav class="navbar navbar-inverse">
+  <div class="container">
+    
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-nav" aria-expanded="false">
+        <span class="sr-only">Toggle navigation</span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+      </button>
+      <a class="navbar-brand" href="#">Home</a>
+    </div>
+
+   
+    <div class="collapse navbar-collapse" id="app-nav">
+      <ul class="nav navbar-nav">
+        <li><a href="#">categories</a></li>
+     
+      </ul>
+      
+      <ul class="nav navbar-nav navbar-right">
+        
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">mohamed <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">edit profile</a></li>
+            <li><a href="#">setting</a></li>
+            <li><a href="#">log out</a></li>
+            
+          </ul>
+        </li>
+      </ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
